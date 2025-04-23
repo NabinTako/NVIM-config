8e9ed3dcd1facb8e584bda6527d1a97959f80792
nabintako <nabintako.teslatech@gmail.com>
2025-04-23 12:15:49 +0545

    transparent BG

--- init.lua
+++ init.lua
@@ -3,6 +3,54 @@ require("config.keymaps")
 require("config.lazy")
 require("config.autocmd")
 
+-- INFO: --vim configs --
+-- -- Transparent backgrounds --
+local hl = vim.api.nvim_set_hl
+
+hl(0, "Normal", { bg = "none" }) -- normal text
+hl(0, "NormalNC", { bg = "NONE" })
+-- hl(0, "CursorLine", { bg = "NONE" }) -- Cursor line number
+hl(0, "NormalFloat", { bg = "none" }) -- floating window
+hl(0, "NeoTreeNormal", { bg = "none" }) --neo tree
+hl(0, "NeoTreeNormalNC", { bg = "none" })
+hl(0, "LineNr", { bg = "none" }) -- line number
+hl(0, "FloatBorder", { fg = "NONE", bg = "NONE" }) -- popup border
+hl(0, "FloatTitle", { fg = "NONE", bg = "NONE" }) -- Floating window title
+hl(0, "Search", { bg = "NONE", fg = "#fab387" })
+hl(0, "QuickFixLine", { bg = "NONE", fg = "#ff6c6b" })
+
+-- Pmenu (popup completion menu)
+hl(0, "Pmenu", { bg = "NONE" })
+hl(0, "PmenuSel", { bg = "NONE" })
+hl(0, "PmenuSbar", { bg = "NONE" })
+hl(0, "PmenuThumb", { bg = "NONE" })
+
+-- LSP-related
+hl(0, "LspFloatWinNormal", { bg = "NONE" })
+hl(0, "LspInfoBorder", { fg = "NONE", bg = "NONE" })
+
+-- Main floating window
+hl(0, "TelescopeNormal", { bg = "NONE" })
+
+-- Borders
+hl(0, "TelescopeBorder", { bg = "NONE", fg = "NONE" })
+
+-- Prompt title area
+hl(0, "TelescopePromptTitle", { bg = "NONE", fg = "NONE" })
+hl(0, "TelescopeResultsTitle", { bg = "NONE", fg = "NONE" })
+hl(0, "TelescopePreviewTitle", { bg = "NONE", fg = "NONE" })
+
+-- Preview + results + prompt areas
+hl(0, "TelescopePromptNormal", { bg = "NONE" })
+hl(0, "TelescopeResultsNormal", { bg = "NONE" })
+hl(0, "TelescopePreviewNormal", { bg = "NONE" })
+
+-- Prompt border
+hl(0, "TelescopePromptBorder", { bg = "NONE", fg = "NONE" })
+hl(0, "TelescopeResultsBorder", { bg = "NONE", fg = "NONE" })
+hl(0, "TelescopePreviewBorder", { bg = "NONE", fg = "NONE" })
+-- INFO: -- End of vim configs --
+--
 -- vim.cmd.colorscheme("kanagawa-wave")
 -- local win_op = function()
 -- 	local Snacks = require("plugins.snacks")
